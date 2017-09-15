7388ebfb1c2ba16bb7d1b6f04e1cb3c6bea15a16
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-15 11:38:30 -0400

    no longer going to host project backends here

--- app/server/index.js
+++ app/server/index.js
@@ -11,23 +11,7 @@ const path = require('path');
 
 const app = express();
 
-const bacon = require('bacon');
-
-
-const BACON_URL = /.*\/projects\/bacon\/.*$/;
-const FILE_NAME = /(.+)(\/[^/]+)$/;
-
-app.use('*', (req, res, next) => {
-	console.log('in portfolio server, ', req.baseUrl, ' : ', req.url, ' : ', req.path, ' : ', req.headers.referer);
-	if (BACON_URL.test(req.headers.referer) && !BACON_URL.test(req.baseUrl)) {
-		console.log('redirecting');
-		res.redirect('/projects/bacon' + req.baseUrl);
-	}
-	
-	next();
-});
-
-app.use('/projects/bacon', bacon);
+app.use(express.static(path.join(__dirname, '../client/')));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../index.html'));
