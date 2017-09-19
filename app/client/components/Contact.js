cd8148e0f9c140ce5d1960b0c0f31b7e6c772d38
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-19 14:12:34 -0400

    contact submission forms are now received by the server and response is handled appropriately

--- app/client/components/Contact.js
+++ app/client/components/Contact.js
@@ -12,22 +12,14 @@ class Contact extends React.Component {
 		super(props);
 		
 		this.state = {
-			emailClass: ''
 		};
-	}
-
-
-	emailChange() {
-		if (!this.state.emailClass) {
-			this.setState({ emailClass: 'modified' });
-		}
 	}
 
 
 	render() {
 		return (
 			<div id='contact'>
-				<form action='/email' method='post' encType='text/plain'>
+				<form action='/email' method='post'>
 					<input id='name' type='text' name='name' placeholder='Name *' required></input>
 					<input id='email' type='email' name='email' placeholder='Email address *' required></input>
 					<textarea id='message' name='message' placeholder='Message' required></textarea>
--- app/server/index.js
+++ app/server/index.js
@@ -5,31 +5,23 @@
  **/
 
 
-const repl = require('repl');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../')));
-app.use('/', (req, res, next) => {
-	if (req.url == '/email') {
-		console.log(req.originalUrl);
-		repl.start().context.req = req;
-	}
 
-	next();
-});
+app.use('/email', bodyParser.urlencoded({ extended: true }));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-// app.post('/email', req => {
-// 	 console.log(req.headers);
-// 	 repl.start().context.req = req;
-// });
+app.post('/email', (req, res) => {
+	res.redirect(301, '/contact');
+});
 
 app.listen(4000);
 
