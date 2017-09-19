cddda00b09aa56604036d1a2a125e5211c745d03
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-19 14:39:41 -0400

    server now forwards emails sent through contact page

--- app/server/index.js
+++ app/server/index.js
@@ -7,21 +7,51 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'nicholas.zeiss.portfolio@gmail.com',
+		pass: require('./auth')
+	}
+});
+
+const messageDefaults = {
+	from: 'nicholas.zeiss.portfolio@gmail.com',
+	to: 'nicholas.zeiss@gmail.com'
+};
 
 app.use(express.static(path.join(__dirname, '../')));
 
 app.use('/email', bodyParser.urlencoded({ extended: true }));
 
+
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
+
 app.post('/email', (req, res) => {
+	let message = Object.assign({
+		subject: `${req.body.name}: ${req.body.email}`,
+		text: req.body.message
+	}, messageDefaults);
+
+	transporter.sendMail(message, (err, info) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(info);
+		}
+	});
+
 	res.redirect(301, '/contact');
 });
+
 
 app.listen(4000);
 
